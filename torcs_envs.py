97c5ae36c43349b606ea6f25a13ec4fb0aac1fcf
kivancguckiran <kivancguckiran@gmail.com>
2019-05-18 23:18:01 +0300

    added env

--- torcs_envs.py
+++ torcs_envs.py
@@ -16,7 +16,7 @@ class DefaultEnv(TorcsEnv):
         return self.action_space.shape[0]
 
 
-class NoBrakeEnv(DefaultEnv):
+class NoBrakeNoBackwardsEnv(DefaultEnv):
     def __init__(self, port=3101):
         super().__init__(port)
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(2,))
@@ -25,3 +25,10 @@ class NoBrakeEnv(DefaultEnv):
         env_u = u.copy()
         env_u[1] = (env_u[1] + 1) / 2
         return super().step(np.concatenate((env_u, [-1])))
+
+class HalfBrakeNoBackwardsEnv(DefaultEnv):
+    def step(self, u):
+        env_u = u.copy()
+        env_u[1] = (env_u[1] + 1) / 2
+        env_u[2] = (env_u[2] - 1) / 2
+        return super().step(env_u)
