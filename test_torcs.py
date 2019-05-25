1d58a319c55e60b5f23d5be7caf265637bf0f263
kivancguckiran <kivancguckiran@gmail.com>
2019-05-25 18:21:48 +0300

    test torcs minor additions

--- test_torcs.py
+++ test_torcs.py
@@ -14,15 +14,13 @@ parser.add_argument(
 parser.add_argument(
     "--load-from", type=str, help="load the saved model and optimizer at the beginning")
 parser.add_argument(
-    "--track", type=str, default="none", help="track name")
-parser.add_argument(
     "--max-episode-steps", type=int, default=10000, help="max episode step")
 parser.add_argument(
     "--test", dest="test", action="store_true", help="test mode (no training)")
 parser.add_argument(
     "--use-filter", dest="filter", action="store_true", help="apply filter to observations")
 parser.add_argument(
-    "--port", dest="post", type=str, help="port")
+    "--port", dest="port", type=str, help="port")
 
 parser.set_defaults(test=True)
 parser.set_defaults(load_from=None)
@@ -35,30 +33,22 @@ def main():
 
     if args.algo == "dqn9":
         env = torcs.DiscretizedOldEnv(nstack=1,
-                                      reward_type=args.reward_type,
-                                      track=args.track,
                                       filter=filter,
                                       client_mode=True,
                                       port=args.port)
     elif args.algo == "dqn21":
         env = torcs.DiscretizedEnv(nstack=1,
-                                   reward_type=args.reward_type,
-                                   track=args.track,
                                    filter=filter,
                                    action_count=21,
                                    client_mode=True,
                                    port=args.port)
     elif args.algo == "sac":
         env = torcs.BitsPiecesContEnv(nstack=4,
-                                      reward_type=args.reward_type,
-                                      track=args.track,
                                       filter=filter,
                                       client_mode=True,
                                       port=args.port)
     elif args.algo == "sac-lstm":
         env = torcs.BitsPiecesContEnv(nstack=1,
-                                      reward_type=args.reward_type,
-                                      track=args.track,
                                       filter=filter,
                                       client_mode=True,
                                       port=args.port)
